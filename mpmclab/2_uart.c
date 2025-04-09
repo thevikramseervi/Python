28cd77785b0dbff40653b8ec7d7a720dc9c9311e
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-04-09 13:44:03 +0000

    02. UART

--- mpmclab/2_uart.c
+++ mpmclab/2_uart.c
@@ -10,10 +10,9 @@ int main(void) {
     U0LCR = 0X00000003;
     while (1) {
         while (*msg != 0x00) {
-            while (!(U0LSR & 0X20)) {
-                U0THR = *msg;
-                msg++;
-            }
+            while (!(U0LSR & 0X20));
+            U0THR = *msg;
+            msg++;
         }
     }
 }
